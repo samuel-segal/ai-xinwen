6fbb177f0dafcd54f56b1c1d5ad3770115d7d69c
Samuel Segal <samuel.b.segal@gmail.com>
2025-06-22 19:21:48 -0400

    Refactor to user_id

--- student_word_db.py
+++ student_word_db.py
@@ -5,34 +5,34 @@ client = boto3.client(
 )
 
 
-def get_student_words(student_id: str) -> str:
+def get_student_words(user_id: str) -> str:
     student_info_request = client.get_item(
         TableName = 'student_info',
         Key = {
-            'student_id': {
-                'S': student_id
+            'user_id': {
+                'S': user_id
             }
         },
-        ProjectionExpression = 'student_id, known_words'
+        ProjectionExpression = 'user_id, known_words'
     )
 
     student_info = student_info_request['Item']
 
     return student_info['known_words']['SS']
 
-def initialize_student_words(student_id: str, resource_name: str) -> None:
+def initialize_student_words(user_id: str, resource_name: str) -> None:
     with open(resource_name, 'r', encoding='utf-8') as file:
         known_words = file.read().splitlines()
-        update_student_words(student_id, known_words)
+        update_student_words(user_id, known_words)
 
 #Necessary for a clean reset, for larger it may be more efficient
 #To simply add the values on
-def update_student_words(student_id: str, known_words: list[str]) -> None:
+def update_student_words(user_id: str, known_words: list[str]) -> None:
     client.update_item(
         TableName = 'student_info',
         Key = {
-            'student_id': {
-                'S': student_id
+            'user_id': {
+                'S': user_id
             }
         },
         UpdateExpression = 'SET #KW = :w',
@@ -44,15 +44,15 @@ def update_student_words(student_id: str, known_words: list[str]) -> None:
         }
     )
 
-def get_student_articles(student_id: str):
+def get_student_articles(user_id: str):
     student_info_request = client.get_item(
         TableName = 'student_info',
         Key = {
-            'student_id': {
-                'S': student_id
+            'user_id': {
+                'S': user_id
             }
         },
-        ProjectionExpression = 'student_id, translated_articles'
+        ProjectionExpression = 'user_id, translated_articles'
     )
 
     student_info = student_info_request['Item']
